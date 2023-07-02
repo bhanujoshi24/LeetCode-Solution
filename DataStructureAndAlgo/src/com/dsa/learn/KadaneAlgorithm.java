1c7dfd372f8479209dd41631a4e6b4d29693680f
bhanujoshi24 <jbhanu244@gmail.com>
2023-07-02 22:23:42 +0530

    Added Better Approach to Kadane

--- DataStructureAndAlgo/src/com/dsa/learn/KadaneAlgorithm.java
+++ DataStructureAndAlgo/src/com/dsa/learn/KadaneAlgorithm.java
@@ -8,7 +8,9 @@ public class KadaneAlgorithm {
 		// Brute Force Approach
 		System.out.println("Brute Force Approach = "+getMaxSumBrute(arr));
 		// Optimal Approach
-		System.out.println("Optimal Approach="+getMaxOptimal(arr));
+		System.out.println("Better Approach = "+getMaxSumBetter(arr));
+		// Optimal Approach
+		System.out.println("Optimal Approach = "+getMaxOptimal(arr));
 	}
 
 	public static int getMaxOptimal(int[] arr) {
@@ -38,4 +40,16 @@ public class KadaneAlgorithm {
 		return max_sum;
 	}
 
+	public static int getMaxSumBetter(int[] arr) {
+		int max_sum = 0, sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			sum = 0;
+			for (int j = i; j < arr.length; j++) {
+				sum+=arr[j];
+				max_sum = Math.max(max_sum, sum);
+			}
+		}
+		return max_sum;
+	}
+	
 }
